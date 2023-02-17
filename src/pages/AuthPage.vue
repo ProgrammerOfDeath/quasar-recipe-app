267587bf5f682e3b26b1d104948e2ac9a489fa81
ProgrammerOfDeath <rene.hotho@gmail.com>
2023-02-17 14:43:16 +0100

    feat: create sign up form for testing

--- src/pages/AuthPage.vue
+++ src/pages/AuthPage.vue
@@ -1,21 +1,63 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { QPage, QForm, QInput } from 'quasar';
+import { QPage, QForm, QInput, QBtn, useQuasar } from 'quasar';
+import { supabase } from 'src/lib/supabaseClient';
+import { AuthError } from '@supabase/gotrue-js';
 
-const name = ref('');
+const email = ref('');
+const password = ref('');
+
+const $q = useQuasar();
+
+const createUser = async () => {
+  if (email.value && password.value) {
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        email: email.value,
+        password: password.value,
+      });
+
+      if (data.session && data.user) {
+        $q.notify({
+          color: 'green-4',
+          message: 'Account created',
+        });
+      }
+
+      if (error) {
+        throw error;
+      }
+    } catch (e) {
+      if (e instanceof AuthError) {
+        $q.notify({
+          message: e.message,
+          color: 'red',
+        });
+      }
+    }
+  }
+};
 </script>
 
 <template>
   <QPage class="row items-center justify-evenly">
-    <QForm>
-      <QInput :model-value="name" outlined label="Name" hint="Your username"></QInput>
+    <QForm @submit="createUser()">
+      <QInput
+        v-model="email"
+        outlined
+        label="Name"
+        hint="Your username"
+        :rules="[val => val?.length > 0 || 'Type in a name']"
+      ></QInput>
       <QInput
         class="q-mt-md"
-        :model-value="name"
+        v-model="password"
         outlined
         label="Password"
         hint="Your password"
+        :rules="[val => val?.length > 0 || 'Type in a password']"
       ></QInput>
+      <QBtn class="q-mt-md" type="submit" label="Submit" color="primary"></QBtn>
     </QForm>
   </QPage>
 </template>
