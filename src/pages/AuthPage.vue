d662326b4913e8466691639b1d990629edf2f4fb
ProgrammerOfDeath <rene.hotho@gmail.com>
2023-02-17 11:05:40 +0100

    init project

---
+++ src/pages/AuthPage.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { QPage, QForm, QInput } from 'quasar';
+
+const name = ref('');
+</script>
+
+<template>
+  <QPage class="row items-center justify-evenly">
+    <QForm>
+      <QInput
+        :model-value="name"
+        outlined
+        label="Name"
+        hint="Your username"
+      ></QInput>
+      <QInput
+        class="q-mt-md"
+        :model-value="name"
+        outlined
+        label="Password"
+        hint="Your password"
+      ></QInput>
+    </QForm>
+  </QPage>
+</template>
